a8dcb1f292e692ecc951a3244c95cfc6c083063c
Nelson Carabali <ncarabali@intergrupo.com>
2021-03-10 12:30:58 -0500

    Se realiza ajuste con respecto comportamiento de base de datos.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const db = require("./models");
 const params = process.argv;
 
 if (params.length <= 2) {
-  process.exit(0);
+    process.exit(0);
 }
 
 const args = params.slice(2);
@@ -13,76 +13,77 @@ const command = args[0].split(":")[0].substring(2);
 const entity = args[0].split(":")[1];
 
 switch (command) {
-  case CRUD.CREATE:
-    const data = {};
-    args.slice(1).map(arg => {
-      const tmp = arg.split("=");
-      data[tmp[0].substring(2)] = tmp[1];
-    });
-
-    db[entity]
-      .create(data)
-      .then(() => console.log("Contact created!"))
-      .catch(console.log);
-
-    break;
-
-  case CRUD.READ:
-    db[entity]
-      .findAll()
-      .then(console.log)
-      .catch(console.log);
-    break;
-
-  case CRUD.UPDATE:
-    const columnId = args[1].split("=");
-    const columns = args.slice(2);
-
-    db[entity]
-      .findByPk(columnId[1])
-      .then((contact) =>{
-        if(contact){
-          const data = {};
-          columns.map(column => {
-            const tmp = column.split("=");
+    case CRUD.CREATE:
+        const data = {};
+        args.slice(1).map(arg => {
+            const tmp = arg.split("=");
             data[tmp[0].substring(2)] = tmp[1];
-          });
-          db[entity]
-          .update(data, {
-            where: {id: columnId[1]}
-          })
-          .then((result) => console.log('Contact updated!') )
-          .catch(console.log);
-        }
-        else{
-          console.log('Contact not found!');
-          process.exit(0);
-        }
-      })
-      .catch(console.log);
+        });
 
-    break;
+        db[entity]
+            .create(data)
+            .then(() => console.log("Contact created!"))
+            .catch(console.log);
+
+        break;
+
+    case CRUD.READ:
+        db[entity]
+            .findAll()
+            .then(console.log)
+            .catch(console.log);
+        break;
+
+    case CRUD.UPDATE:
+        const columnId = args[1].split("=");
+        const columns = args.slice(2);
+
+        db[entity]
+            .findByPk(columnId[1])
+            .then((contact) => {
+                if (contact) {
+                    const data = {};
+                    columns.map(column => {
+                        const tmp = column.split("=");
+                        data[tmp[0].substring(2)] = tmp[1];
+                    });
+                    db[entity]
+                        .update(data, {
+                            where: { id: columnId[1] }
+                        })
+                        .then((result) => console.log('Contact updated!'))
+                        .catch(console.log);
+                } else {
+                    console.log('Contact not found!');
+                    process.exit(0);
+                }
+            })
+            .catch(console.log);
+
+        break;
 
 
     case CRUD.DELETE:
-      const columnIdDelete = args[1].split("=");
-      db[entity]
-      .findByPk(columnIdDelete[1])
-      .then((contact) => {
-          if(contact){
-            contact.destroy();
-            console.log('Contact destroy!')
-          }
-          else{
-            console.log('Contact not found!');
-            process.exit(0);
-          }
-      })
-      .catch(console.log)
-      break;
+        const columnIdDelete = args[1].split("=");
+        db[entity]
+            .findByPk(columnIdDelete[1])
+            .then((contact) => {
+                if (contact) {
+                    contact.destroy();
+                    console.log('Contact destroy!')
+                } else {
+                    console.log('Contact not found!');
+                    process.exit(0);
+                }
+            })
+            .catch(console.log)
+        break;
 
-  default:
-    console.log("Operation not found!");
+    default:
+        console.log("Operation not found!");
 }
 
-// --create:Contact --firstname=Marluan
+// node . --create:Contact --firstname=Pablo --lastname=Perez --phone=340 45 45 --email=pperez@intergrupo.com 
+// node . --update:Contact --id=2 --firstname=Neron --lastname=Miranda --phone=312343403 --email:nmiranda@intergrupo.com
+// node . --read:Contact
+// node . --delete:Contact --id=5
--- seeders/20200715211949-seed-contact.js
+++ seeders/20200715211949-seed-contact.js
@@ -1,35 +1,40 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-      return queryInterface.bulkInsert('Contacts',[
-          {
-            firstname: 'Nelson',
-            lastname: 'Carabali',
-            phone: '203 202 120',
-            email: 'ncara@int.com.co',
-            createdAt: new Date().toDateString(),
-            updatedAt: new Date().toDateString()
-          },
-          {
-            firstname: 'Alejo',
-            lastname: 'Diaz',
-            phone: '203 567 123',
-            email: 'adiaz@int.com.co',
-            createdAt: new Date().toDateString(),
-            updatedAt: new Date().toDateString()
-          }
+    up: async(queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert('Contacts', [{
+                firstname: 'Nelson',
+                lastname: 'Carabali',
+                phone: '203 202 120',
+                email: 'ncara@int.com.co',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                firstname: 'Alejo',
+                lastname: 'Diaz',
+                phone: '203 567 123',
+                email: 'adiaz@int.com.co',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            },
+            {
+                firstname: 'Juan',
+                lastname: 'Perez',
+                phone: '213 523 133',
+                email: 'jperez@int.com.co',
+                createdAt: new Date().toDateString(),
+                updatedAt: new Date().toDateString()
+            }
+        ]);
+    },
 
-
-      ])
-  },
-
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+    down: async(queryInterface, Sequelize) => {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+    }
 };
